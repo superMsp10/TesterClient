9f7b9ad20ca4d1939db3a4d7fe0c3d575e55c16b
superMsp10 <pandeymahan34@gmail.com>
2017-05-30 14:59:12 -0400

    post

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,70 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.JOptionPane;
 
 public class Main {
+	static String title = "Tester Client";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String username = JOptionPane.showInputDialog(null, "Enter username", title, JOptionPane.QUESTION_MESSAGE);
+		String password = JOptionPane.showInputDialog(null, "Enter password", title, JOptionPane.QUESTION_MESSAGE);
+		login();
 	}
 
+	public static boolean login() {
+		String url = "https://selfsolve.apple.com/wcResults.do";
+
+		try {
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+			//add reuqest header
+			con.setRequestMethod("POST");
+			con.setRequestProperty("User-Agent", "hello");
+			con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+			String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+			// Send post request
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(urlParameters);
+			wr.flush();
+			wr.close();
+
+			int responseCode = con.getResponseCode();
+			System.out.println("\nSending 'POST' request to URL : " + url);
+			System.out.println("Post parameters : " + urlParameters);
+			System.out.println("Response Code : " + responseCode);
+
+			BufferedReader in = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			//print result
+			System.out.println(response.toString());
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
+
+	}
 }
